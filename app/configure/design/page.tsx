82c6e674f32e4037c81a382bcdeee914f8ad7de2
Emmauel Ikechukwu <eik@pml.ac.uk>
2024-05-12 12:06:53 +0100

    added rose desing options

--- app/configure/design/page.tsx
+++ app/configure/design/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
@@ -6,8 +10,28 @@ interface PageProps {
 
 const page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
   // make a db call if needed
-  return <div>page {id}</div>;
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+  return (
+    <DesignConfigurator
+      configId={configuration?.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default page;
