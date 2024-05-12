d37717e25d3295ab9112683e86deba964687e295
Emmauel Ikechukwu <eik@pml.ac.uk>
2024-05-12 15:51:38 +0100

    phone customization feature pick with updating price

--- validators/options-validators.ts
+++ validators/options-validators.ts
@@ -2,6 +2,8 @@
 // bg-zinc-900 border-zinc-900
 // bg-rose-950 border-rose-950
 
+import { PRODUCT_PRICES } from "@/config/products"
+
 export const COLORS = [
     {
         label: "Blue", value: "blue", tw:'blue-950'
@@ -12,4 +14,76 @@ export const COLORS = [
     {
         label: "Rose", value: "rose", tw:'rose-900'
     },
+    {
+        label: "Green", value: "green", tw:'green-700'
+    },
 ] as const
+
+
+export const MODELS = {
+    name: "models", 
+    options: [
+        {
+            label: "Iphone X", 
+            value: "iphonex"
+        },
+        {
+            label: "Iphone 11", 
+            value: "iphone11"
+        },
+        {
+            label: "Iphone 12", 
+            value: "iphone12"
+        },
+        {
+            label: "Iphone 13", 
+            value: "iphone13"
+        },
+        {
+            label: "Iphone 14", 
+            value: "iphone14"
+        },
+        {
+            label: "Iphone 15", 
+            value: "iphone15"
+        },
+    ]
+}as const
+
+
+export const MATERIALS = {
+    name: "material",
+    options: [
+        {
+            label: "Silicone",
+            value: "silicone",
+            description: undefined,
+            price: PRODUCT_PRICES.material.silicone
+        }, 
+        {
+            label: "Soft Polycarbonate",
+            value: "polycarbonate",
+            description: "Scratch-resistant coating",
+            price: PRODUCT_PRICES.material.polycarbonate
+        }, 
+    ]
+} as const
+
+
+export const FINISHES = {
+    name: "finish",
+    options: [
+        {
+            label: "Smooth Finish",
+            value: "smooth",
+            description: undefined,
+            price: PRODUCT_PRICES.finish.smooth
+        }, 
+        {
+            label: "Textured Finish",
+            value: "textured",
+            description: "Soft grippy texture",
+            price: PRODUCT_PRICES.finish.textured
+        }, 
+    ]
+} as const
